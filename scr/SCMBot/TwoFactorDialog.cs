38629cf2d430c6340ecf77958076664bb81fc22e
MADsadFATcat <mad-mouse@mail.ru>
2015-12-10 18:31:00 +0300

    two factor auth / двухфакторная авторизация с мобильного приложения

---
+++ scr/SCMBot/TwoFactorDialog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SCMBot
+{
+
+    public partial class TwoFactorDialog : Form
+    {
+        public string TwoFactorCode
+        {
+            get { return twoFactorCode.Text; }
+            set { twoFactorCode.Text = value; }
+        }
+
+        public TwoFactorDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            return;
+        }
+
+    }
+}
